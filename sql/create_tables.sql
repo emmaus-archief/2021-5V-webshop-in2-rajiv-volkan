93b861686a8ab931ece6db1a47b6307fb135d506
Volkan24 <vkaral04@hotmail.com>
2020-11-25 14:13:45 +0000

    TABELLEN

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,22 @@
-DROP TABLE IF EXISTS products; 
-CREATE TABLE products (
+DROP TABLE IF EXISTS autos; 
+CREATE TABLE autos (
   ID SERIAL PRIMARY KEY,
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2)
+  model_id INTEGER 
+  kleur_id INTEGER 
+);
+
+DROP TABLE IF EXISTS model
+CREATE TABLE  model (
+  ID SERIAL PRIMARY KEY,
+  naam_model TEXT 
+);
+
+DROP TABLE IF EXISTS kleur
+CREATE TABLE kleur(
+    ID SERIAL NUMBER 
+    naam_kleur
 );
