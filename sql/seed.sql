7fe34ac6112b0a137e1307003290412b753a9cb5
Volkan24 <vkaral04@hotmail.com>
2020-12-01 15:25:51 +0000

    seed

--- sql/seed.sql
+++ sql/seed.sql
@@ -4,28 +4,46 @@
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into kleur values(1,"zwart");
-insert into kleur values(2,"wit");
-insert into kleur values(3,"grijs");
-insert into kleur values(4,"rood");
-insert into kleur values(5,"blauw");
-insert into kleur values(6,"groen");
-insert into kleur values(7,"geel");
+insert into kleur(id, kleurnaam) values(1,"zwart");
+insert into kleur(id, kleurnaam) values(2,"wit");
+insert into kleur(id, kleurnaam) values(3,"grijs");
+insert into kleur(id, kleurnaam) values(4,"rood");
+insert into kleur(id, kleurnaam) values(5,"blauw");
+insert into kleur(id, kleurnaam) values(6,"groen");
+insert into kleur(id, kleurnaam) values(7,"geel");
 
 
-insert into merk values (1, "Audi");
-insert into merk values (2, "Mercedes");
-insert into merk values (3, "Nissan");
-insert into merk values (4,"Volkswagen");
-insert into merk values (5, "Lamborghini");
-insert into merk values (6, "Kia");
-insert into merk values (7, "BMW");
-insert into merk values (8, "Opel");
+insert into merk(id, merk_naam) values (1, "Audi");
+insert into merk(id, merk_naam) values (2, "Mercedes");
+insert into merk(id, merk_naam) values (3, "Nissan");
+insert into merk(id, merk_naam) values (4,"Volkswagen");
+insert into merk(id, merk_naam) values (5, "Lamborghini");
+insert into merk(id, merk_naam) values (6, "Kia");
+insert into merk(id, merk_naam) values (7, "BMW");
+insert into merk(id, merk_naam) values (8, "Opel");
 
 
-insert into soort_auto values(1,"Sedan");
-insert into soort_auto values(2,"hatchback");
-insert into soort_auto values(3, "SUV");
-insert into soort_auto values(4,"Sport");
-insert into soort_auto values(5, "Coupé");
-insert into soort_auto values(6, "Stationwagon");
+insert into soort_auto(id, soort_auto_naam) values(1,"Sedan");
+insert into soort_auto(id, soort_auto_naam) values(2,"hatchback");
+insert into soort_auto(id, soort_auto_naam) values(3, "SUV");
+insert into soort_auto(id, soort_auto_naam) values(4,"Sport");
+insert into soort_auto(id, soort_auto_naam) values(5, "Coupé");
+insert into soort_auto(id, soort_auto_naam) values(6, "Stationwagon");
+
+
+
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
+insert into autos(id,code, auto_naam, description, price, kleur_id, merk_id, soort_auto_id) values()
