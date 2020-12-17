4c866b197a842fec5dce0e59a68d6e30f80f4d81
Volkan24 <vkaral04@hotmail.com>
2020-12-17 13:23:44 +0000

    12

--- sql/seed.sql
+++ sql/seed.sql
@@ -32,24 +32,26 @@ insert into soort_auto(id, soort_auto_naam) values(6, 'Stationwagon');
 
 
 
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(1,01-123,'Nissan Qashqai','Mooie stevige grijze auto', 30000,3,3,3 );
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(2, 02-123,'BMW M5', 'Een zwarte auto', 75000,1,7,5);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(3,03-123, 'Lamborghini Aventador', 'Een gele auto', 300000, 7,5,4 );
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(4,04-123,'Kia Picanto', 'Een rode auto', 20000, 4,6,2);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(5,05-123, 'Volkswagen GTI', 'Een witte auto', 35000, 2,4,2);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(6,06-123, 'Nissan GTR R33',  'Een witte auto', 20000,2,3,4);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(7,07-123, 'Kia Niro', 'Een zwarte auto', 30000, 1, 6, 3);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(8,08-123, 'Lamborghini Urus', 'Een blauwe auto', 300000, 5,5,3);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(9, 09-1234, 'Zwarte Mercedes GT63s AMG', 'Mooie zwarte Mercedes', 200000, 1,2,5);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(10,10-1234,'Witte Audi A4', 'Mooie witte Audi', 50000, 2, 1, 6);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(11, 11-1234,'Zwarte Opel Astra','Mooie zwarte Opel', 25000, 1,8, 1 );
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(12, 12-1234, 'Groene Lamborghini Hurucan Perfomante', 'Mooie groene Lamborghini', 250000, 6,5,4);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(13, 13-1234, 'Grijze `Volkswagen Polo', 'Mooie grijze Volkswagen Polo', 25000, 3, 4,2);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(14, 14-1234, 'Zwarte Kia Stonic', 'Mooie zwarte Kia Stonic', 32000, 1, 6, 3);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(15, 15-1234, 'Witte Mercedes c63amg', 'Mooie witte Mercedes c63amg',60000,2,2,5);
+insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(16, 16-1234, 'Blauwe Audi Rs3', 'Mooie blauwe audi Rs3',50000, 5,1,5);
 
 
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(1,01-123,'Nissan Qashqai','Mooie stevige grijze auto', 30.000,3,3,3 );
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(2, 02-123,'BMW M5', 'Een zwarte auto', 75.000,1,7,5);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(3,03-123, 'Lamborghini Aventador', 'Een gele auto', 300.000, 7,5,4 );
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(4,04-123,'Kia Picanto', 'Een rode auto', 20.000, 4,6,2);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(5,05-123, 'Volkswagen GTI', 'Een witte auto', 35.000, 2,4,2);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(6,06-123, 'Nissan GTR R33',  'Een witte auto', 20.000,2,3,4);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(7,07-123, 'Kia Niro', 'Een zwarte auto', 30.000, 1, 6, 3);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(8,08-123, 'Lamborghini Urus', 'Een blauwe auto', 300.000, 5,5,3);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(9, 09-1234, 'Zwarte Mercedes GT63s AMG', 'Mooie zwarte Mercedes', 200.000, 1,2,5);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(10,10-1234,'Witte Audi A4', 'Mooie witte Audi', 50.000, 2, 1, 6);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(11, 11-1234,'Zwarte Opel Astra','Mooie zwarte Opel', 25.000, 1,8, 1 );
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(12, 12-1234, 'Groene Lamborghini Hurucan Perfomante', 'Mooie groene Lamborghini', 250.000, 6,5,4);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(13, 13-1234, 'Grijze `Volkswagen Polo', 'Mooie grijze Volkswagen Polo', 25.000, 3, 4,2);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(14, 14-1234, 'Zwarte Kia Stonic', 'Mooie zwarte Kia Stonic', 32.000, 1, 6, 3);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(15, 15-1234, 'Witte Mercedes c63amg', 'Mooie witte Mercedes c63amg',60.000,2,2,5);
-insert into products(id,code, auto_naam, description, price, kleuren_id, merken_id, soort_auto_id) values(16, 16-1234, 'Blauwe Audi Rs3', 'Mooie blauwe audi Rs3',50.000, 5,1,5);
+
+
 
 
 
